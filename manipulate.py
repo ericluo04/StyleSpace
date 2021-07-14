d59889df89940b6f37b9d6ddcb4e2ce49ac0a447
ericluo04 <39772787+ericluo04@users.noreply.github.com>
2021-07-14 12:05:15 -0400

    Update manipulate.py

--- manipulate.py
+++ manipulate.py
@@ -1,5 +1,4 @@
-
-
+# packages
 import os
 import os.path
 import pickle
@@ -8,7 +7,7 @@ import tensorflow as tf
 from dnnlib import tflib
 from utils.visualizer import HtmlPageVisualizer
 
-
+# visualize a collection of images
 def Vis(bname,suffix,out,rownames=None,colnames=None):
     num_images=out.shape[0]
     step=out.shape[1]
@@ -17,7 +16,6 @@ def Vis(bname,suffix,out,rownames=None,colnames=None):
         colnames=[f'Step {i:02d}' for i in range(1, step + 1)]
     if rownames is None:
         rownames=[str(i) for i in range(num_images)]
-    
     
     visualizer = HtmlPageVisualizer(
       num_rows=num_images, num_cols=step + 1, viz_size=256)
@@ -32,12 +30,10 @@ def Vis(bname,suffix,out,rownames=None,colnames=None):
             image=out[i,k,:,:,:]
             visualizer.set_cell(i, 1+k, image=image)
     
-    # Save results.
+    # save results
     visualizer.save(f'./html/'+bname+'_'+suffix+'.html')
 
-
-
-
+# load latents, values for attribute-specific channels
 def LoadData(img_path):
     tmp=img_path+'S'
     with open(tmp, "rb") as fp:   #Pickling
@@ -59,7 +55,7 @@ def LoadData(img_path):
     
     return dlatents,s_names,mindexs,pindexs,m,std
 
-
+# load StyleGAN2 model
 def LoadModel(model_path,model_name):
     # Initialize TensorFlow.
     tflib.init_tf()
@@ -69,10 +65,9 @@ def LoadModel(model_path,model_name):
     Gs.print_layers()
     return Gs
 
+# convert minibatch of images from float32 to uint8 with configurable dynamic range
+# can be used as an OUTPUT transformation for Network.run()
 def convert_images_to_uint8(images, drange=[-1,1], nchw_to_nhwc=False):
-    """Convert a minibatch of images from float32 to uint8 with configurable dynamic range.
-    Can be used as an output transformation for Network.run().
-    """
     if nchw_to_nhwc:
         images = np.transpose(images, [0, 2, 3, 1])
     
@@ -83,32 +78,37 @@ def convert_images_to_uint8(images, drange=[-1,1], nchw_to_nhwc=False):
     images=images.astype('uint8')
     return images
 
-
+# convert minibatch of images from uint8 to float32 with configurable dynamic range
+# can be used as an INPUT transformation for Network.run()
 def convert_images_from_uint8(images, drange=[-1,1], nhwc_to_nchw=False):
-    """Convert a minibatch of images from uint8 to float32 with configurable dynamic range.
-    Can be used as an input transformation for Network.run().
-    """
     if nhwc_to_nchw:
         images=np.rollaxis(images, 3, 1)
     return images/ 255 *(drange[1] - drange[0])+ drange[0]
 
-
+# key class
 class Manipulator():
     def __init__(self,dataset_name='ffhq'):
+        # data paths
         self.file_path='./'
         self.img_path=self.file_path+'npy/'+dataset_name+'/'
         self.model_path=self.file_path+'model/'
         self.dataset_name=dataset_name
         self.model_name=dataset_name+'.pkl'
         
-        self.alpha=[0] #manipulation strength 
-        self.num_images=10
-        self.img_index=0  #which image to start 
+        # list of seeds to generate
+        self.seed_list = [0, 1, 2, 3, 4, 5]
+        # number of images to generate
+        self.num_images=len(self.seed_list)
+        # manipulation strength
+        self.alpha=[0]
+        # visualization dimension
         self.viz_size=256
-        self.manipulate_layers=None #which layer to manipulate, list
+        # which layer to manipulate (list)
+        self.manipulate_layers=None 
         
+        # load latents, attribute-specific channel values
         self.dlatents,self.s_names,self.mindexs,self.pindexs,self.code_mean,self.code_std=LoadData(self.img_path)
-        
+        # load StyleGAN2 model
         self.sess=tf.InteractiveSession()
         init = tf.global_variables_initializer()
         self.sess.run(init)
@@ -118,6 +118,7 @@ class Manipulator():
         self.Vis=Vis
         self.noise_constant={}
         
+        # style space
         for i in range(len(self.s_names)):
             tmp1=self.s_names[i].split('/')
             if not 'ToRGB' in tmp1:
@@ -126,7 +127,7 @@ class Manipulator():
                 tmp1='/'.join(tmp1)
                 tmp=(1,1,size,size)
                 self.noise_constant[tmp1]=np.random.random(tmp)
-        
+        # latent seeds
         tmp=self.Gs.components.synthesis.input_shape[1]
         d={}
         d['G_synthesis_1/dlatents_in:0']=np.zeros([1,tmp,512])
@@ -138,12 +139,10 @@ class Manipulator():
         self.fmt = dict(func=tflib.convert_images_to_uint8, nchw_to_nhwc=True)
         self.img_size=self.Gs.output_shape[-1]
     
+    # generate image (allow for manipulations in style space)
     def GenerateImg(self,codes):
-        
-
         num_images,step=codes[0].shape[:2]
 
-            
         out=np.zeros((num_images,step,self.img_size,self.img_size,3),dtype='uint8')
         for i in range(num_images):
             for k in range(step):
@@ -157,17 +156,14 @@ class Manipulator():
                 out[i,k,:,:,:]=image[0]
         return out
     
-    
-    
+    # key helper function to manipulate style space
     def MSCode(self,dlatent_tmp,boundary_tmp):
-        
         step=len(self.alpha)
         dlatent_tmp1=[tmp.reshape((self.num_images,-1)) for tmp in dlatent_tmp]
         dlatent_tmp2=[np.tile(tmp[:,None],(1,step,1)) for tmp in dlatent_tmp1] # (10, 7, 512)
 
         l=np.array(self.alpha)
-        l=l.reshape(
-                    [step if axis == 1 else 1 for axis in range(dlatent_tmp2[0].ndim)])
+        l=l.reshape([step if axis == 1 else 1 for axis in range(dlatent_tmp2[0].ndim)])
         
         if type(self.manipulate_layers)==int:
             tmp=[self.manipulate_layers]
@@ -188,7 +184,7 @@ class Manipulator():
             codes.append(dlatent_tmp2[i].reshape(tmp))
         return codes
     
-    
+    # edit image in style space
     def EditOne(self,bname,dlatent_tmp=None):
         if dlatent_tmp==None:
             dlatent_tmp=[tmp[self.img_index:(self.img_index+self.num_images)] for tmp in self.dlatents]
@@ -202,17 +198,16 @@ class Manipulator():
                 boundary_tmp.append(tmp[bname])
         
         codes=self.MSCode(dlatent_tmp,boundary_tmp)
-            
         out=self.GenerateImg(codes)
         return codes,out
     
+    # edit image in style space (ONLY manipulating 1 layer and 1 channel)
     def EditOneC(self,cindex,dlatent_tmp=None): 
         if dlatent_tmp==None:
             dlatent_tmp=[tmp[self.img_index:(self.img_index+self.num_images)] for tmp in self.dlatents]
-        
         boundary_tmp=[[] for i in range(len(self.dlatents))]
         
-        #'only manipulate 1 layer and one channel'
+        # check function condition
         assert len(self.manipulate_layers)==1 
         
         ml=self.manipulate_layers[0]
@@ -225,54 +220,27 @@ class Manipulator():
         out=self.GenerateImg(codes)
         return codes,out
     
-        
+    # convert W space to S space
     def W2S(self,dlatent_tmp):
-        
         all_s = self.sess.run(
             self.s_names,
             feed_dict={'G_synthesis_1/dlatents_in:0': dlatent_tmp})
         return all_s
-        
-    
-    
-    
-    
-    
 
-
-#%%
+# boilerplate code (only runs if file passed as command)
 if __name__ == "__main__":
-    
-    
+    # load StyleGAN2 for faces
     M=Manipulator(dataset_name='ffhq')
     
-    
-    #%%
+    # set list of strength levels
     M.alpha=[-5,0,5]
-    M.num_images=20
+    # set list of seeds for generation
+    M.seed_list=[0, 1, 2, 3, 4]
+    # style channels to manipulate
     lindex,cindex=6,501
     
+    # manipulate
     M.manipulate_layers=[lindex]
     codes,out=M.EditOneC(cindex) #dlatent_tmp
     tmp=str(M.manipulate_layers)+'_'+str(cindex)
     M.Vis(tmp,'c',out)
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-
-
-
-
